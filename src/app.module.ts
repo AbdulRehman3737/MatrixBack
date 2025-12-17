8e0a7bca835f76c413cba3478011b652627f3497
AbdulRehman3737 <abdurehman917@gmail.com>
2025-12-18 00:51:24 +0500

    Added health api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StripeModule } from './stripe/stripe.module';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), StripeModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
